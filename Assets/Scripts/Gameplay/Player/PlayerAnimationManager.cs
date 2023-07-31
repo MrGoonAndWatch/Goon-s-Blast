b865718e23330607445ceb4f7ae7b87ece902d0e
Goon <mrgoonandwatch@gmail.com>
2023-07-30 20:07:49 -0400

    Fix pickup animation getting stuck, remove safety code for unstick logic.

--- Assets/Scripts/Gameplay/Player/PlayerAnimationManager.cs
+++ Assets/Scripts/Gameplay/Player/PlayerAnimationManager.cs
@@ -8,20 +8,6 @@ public class PlayerAnimationManager : MonoBehaviour
 
     private bool _isPickingUp;
 
-    private const float PickupCancelTime = 1.1f;
-    private float _remainingPickupTime;
-
-    private void Update()
-    {
-        // TODO: Sometimes the pickup animation doesn't play and without this code the player gets stuck (seems to happen when spamming the pickup button?)
-        if (_remainingPickupTime > 0)
-        {
-            _remainingPickupTime -= Time.deltaTime;
-            if(_remainingPickupTime <= 0)
-                OnPickupEnd();
-        }
-    }
-
     public bool CanMove()
     {
         return !_isPickingUp;
@@ -38,12 +24,10 @@ public class PlayerAnimationManager : MonoBehaviour
 
         _isPickingUp = true;
         _animationController.SetBool(GameConstants.AnimationVariables.PickingUp, true);
-        _remainingPickupTime = PickupCancelTime;
     }
 
     public void OnPickupEnd()
     {
-        _remainingPickupTime = 0;
         _animationController.SetBool(GameConstants.AnimationVariables.PickingUp, false);
         _isPickingUp = false;
     }
